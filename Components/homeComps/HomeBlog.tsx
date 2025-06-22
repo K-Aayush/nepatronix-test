c9170c4a883198a96a559df5438b81e819459146
Aayush <ayushkarkee6@gmail.com>
2025-06-22 15:31:28 +0545

    updated blogs page

--- Components/homeComps/HomeBlog.tsx
+++ Components/homeComps/HomeBlog.tsx
@@ -5,21 +5,20 @@ import BlogCard from "../Reusables/BlogCard";
 
 const HomeBlog = async () => {
   const data = await getLists("blogs", 0, 18);
+  const limitedData = data.slice(0, 8);
 
   return (
-    <section className="w-full py-[15px] text-[20px]  bg-gradient-to-l from-red-200 to-blue-200 text-center">
+    <section className="w-full py-[15px] text-[20px]  bg-gradient-to-tl from-purple-600 to-indigo-500 text-center">
       <>
-          <br />
-          <br />
-          <h2 className="font-bold text-8xl text-[#2c1b3d]">
-            Explore Our Blogs
-          </h2>
-          <br />
-          <br />
-        </>
+        <br />
+        <br />
+        <h2 className="font-bold text-8xl text-[#2c1b3d]">Explore Our Blogs</h2>
+        <br />
+        <br />
+      </>
 
       <div className="w-full px-[40px] flex justify-center gap-[30px] flex-wrap">
-        {data?.map((item: any, index: number) => (
+        {limitedData?.map((item: any, index: number) => (
           <BlogCard key={index} data={item} />
         ))}
       </div>
