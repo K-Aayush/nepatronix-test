20c6a18a6159bda487967f5b639aa562455ba098
Aayush <ayushkarkee6@gmail.com>
2025-06-26 14:44:19 +0545

    completed ai assisted diagonis page

---
+++ app/static/ai-assisted-diagnostics/page.tsx
@@ -0,0 +1,8 @@
+import AIAssistedDiagnosisPage from "@/Components/static/AIAssistedDiagnosisPage";
+import React from "react";
+
+const page = () => {
+  return <AIAssistedDiagnosisPage />;
+};
+
+export default page;
