7ae43036e5f8fb0400a522ce94c8a778c545cd60
Aayush <ayushkarkee6@gmail.com>
2025-06-29 08:38:32 +0545

    updated rnd security defense page

--- app/static/rnd-security-defense/page.tsx
+++ app/static/rnd-security-defense/page.tsx
@@ -1,11 +1,8 @@
+import RnDSecurityDefensePage from "@/Components/static/RnDSecurityDefensePage";
 import React from "react";
 
 const page = () => {
-  return (
-    <div className="flex items-center justify-center min-h-screen text-center text-white bg-gray-800 text-7xl">
-      Comming Soon...
-    </div>
-  );
+  return <RnDSecurityDefensePage />;
 };
 
 export default page;
