1cb467f188a5d11e204359f1fcd779387e154c65
Aayush <ayushkarkee6@gmail.com>
2025-06-29 10:17:25 +0545

    completed agentic ai development

--- app/static/agentic-ai-development/page.tsx
+++ app/static/agentic-ai-development/page.tsx
@@ -1,11 +1,8 @@
+import AgenticAIDevelopmentPage from "@/Components/static/AgenticAIDevelopmentPage";
 import React from "react";
 
 const page = () => {
-  return (
-    <div className="flex items-center justify-center min-h-screen text-center text-white bg-gray-800 text-7xl">
-      Comming Soon...
-    </div>
-  );
+  return <AgenticAIDevelopmentPage />;
 };
 
 export default page;
